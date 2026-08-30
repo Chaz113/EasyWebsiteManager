5abb0e4328b0b6c0e35622fc0c4dc38c8715dd8e
chaz113 <chazk@sasktel.net>
2026-08-29 22:07:08 -0600

    19.7A.6d.4 — Android switch-track rendering.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 
+#if ANDROID
+using Android.Content.Res;
+using Microsoft.Maui.Handlers;
+#endif
 
 namespace EasyWebsiteManager
 {
@@ -8,16 +12,66 @@ namespace EasyWebsiteManager
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont(
+                        "OpenSans-Regular.ttf",
+                        "OpenSansRegular");
+
+                    fonts.AddFont(
+                        "OpenSans-Semibold.ttf",
+                        "OpenSansSemibold");
                 });
 
+#if ANDROID
+            SwitchHandler.Mapper.AppendToMapping(
+                "EasyWebsiteManagerAndroidSwitch",
+                (handler, view) =>
+                {
+                    var platformSwitch =
+                        handler.PlatformView;
+
+                    var states = new[]
+                    {
+                        new[]
+                        {
+                            Android.Resource.Attribute.StateChecked
+                        },
+                        new[]
+                        {
+                            -Android.Resource.Attribute.StateChecked
+                        }
+                    };
+
+                    var trackColors = new[]
+                    {
+                        unchecked((int)0xFF3478F6), // ON blue
+                        unchecked((int)0xFF667085)  // OFF grey
+                    };
+
+                    var thumbColors = new[]
+                    {
+                        unchecked((int)0xFFFFFFFF), // ON white
+                        unchecked((int)0xFFFFFFFF)  // OFF white
+                    };
+
+                    platformSwitch.TrackTintList =
+                        new ColorStateList(
+                            states,
+                            trackColors);
+
+                    platformSwitch.ThumbTintList =
+                        new ColorStateList(
+                            states,
+                            thumbColors);
+                });
+#endif
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
